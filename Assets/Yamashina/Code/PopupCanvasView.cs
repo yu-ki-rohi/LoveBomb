ca4e14240f22259a387b1e82148e04a8b558b1dd
maho0628 <149309001+maho0628@users.noreply.github.com>
2026-01-23 20:06:00 +0900

    Revert "ステージ選択画面改良"
    This reverts commit 074c9be36ecec12dc959d1bf772c3bbad62a9d49.

--- Assets/Yamashina/Code/PopupCanvasView.cs
+++ Assets/Yamashina/Code/PopupCanvasView.cs
@@ -1,17 +1,8 @@
-using TMPro;
 using UnityEngine;
 
 public class PopupCanvasView : MonoBehaviour
 {
     public Transform backgroundRoot;
     public Transform controlRoot;
-
-    [SerializeField] private TextMeshProUGUI stageText;
-
-    public TextMeshProUGUI GetStageText()
-    {
-        return stageText;   
-    }
-
 }
 
