7cca7ca3c824543e6d9b505b5822e06674149cc3
maho0628 <149309001+maho0628@users.noreply.github.com>
2026-01-23 20:07:01 +0900

    ステージ選択画面改良やり直し

--- Assets/Yamashina/Code/PopupCanvasView.cs
+++ Assets/Yamashina/Code/PopupCanvasView.cs
@@ -1,8 +1,17 @@
+using TMPro;
 using UnityEngine;
 
 public class PopupCanvasView : MonoBehaviour
 {
     public Transform backgroundRoot;
     public Transform controlRoot;
+
+    [SerializeField] private TextMeshProUGUI stageText;
+
+    public TextMeshProUGUI GetStageText()
+    {
+        return stageText;   
+    }
+
 }
 
