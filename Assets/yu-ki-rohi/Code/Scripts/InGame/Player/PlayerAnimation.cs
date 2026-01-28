5f4e06bdc5c17182fd8e81d971b6bd3fd768252e
yu-ki-rohi <yukihiro4924@gmail.com>
2026-01-29 03:18:10 +0900

    いくつか調整

--- Assets/yu-ki-rohi/Code/Scripts/InGame/Player/PlayerAnimation.cs
+++ Assets/yu-ki-rohi/Code/Scripts/InGame/Player/PlayerAnimation.cs
@@ -28,7 +28,8 @@ public class PlayerAnimation : NormalPlayerComponent
 
     public void SetShootStage(int index)
     {
-        animator?.SetInteger("ShootStage", index);
+        if (DebugMessenger.NullCheckError(animator)) { return; }
+        animator.SetInteger("ShootStage", index);
     }
 
     // IUpdatable‚É‚æ‚Á‚Ä•ÛØ‚³‚ê‚Ä‚¢‚éƒƒ\ƒbƒh
@@ -220,7 +221,10 @@ public class PlayerAnimation : NormalPlayerComponent
         }
         finally
         {
-            spriteRenderer.enabled = true;
+            if(!DebugMessenger.NullCheckError(spriteRenderer)) 
+            {
+                spriteRenderer.enabled = true;
+            }
         }
     }
 }
