38a824971e55d61d56608561f9f3750295c0114c
maho0628 <149309001+maho0628@users.noreply.github.com>
2026-01-29 02:14:14 +0900

    SEのEnum更新中

--- Assets/Yamashina/Code/SE/SEName.cs
+++ Assets/Yamashina/Code/SE/SEName.cs
@@ -4,18 +4,34 @@
 /// </summary>
 internal enum SEName
 {
-    Arrow,
-    Damage,
-    Charge,
-    BombArrow,
-    Noroi,
-    Kiken,
-    Time1,
-    GetItem,
-    Click,
-    MouseOver,
-    Finish,
+    ArrowShot,//矢を射る音
+    BowStringRelease, //弦を弾く音
 
+    Damage,//通常被弾音
+    EnemyDamage,//敵被弾音
+    BellRing,//ベルを鳴らす音
+    Sphere, //スフィア使用時
+    FeatherPenWrite,//羽ペン使用時
+    Charge,//チャージ完了音
+    AddScore,//スコア加算音
+    DecreaseScore,//スコア減少音
+    GetItem,//アイテム入手音
+
+    BombArrow,//ボム矢発射音
+    Click,　//クリック時のSE
+
+
+    /// <summary>
+    /// 恐らく必要？
+    /// </summary>
+    MouseOver,//マウスオーバー時のSE（？）
+
+
+    Noroi, //心の核にエネミー到着(?)
+    Kiken,//心の核危険(?)
+    Time1,//時間経過音
+   
+    Finish,//終了時のSE
 }
 
 
