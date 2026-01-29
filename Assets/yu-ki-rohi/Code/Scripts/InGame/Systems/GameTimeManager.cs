0b4a1e3449b9cbad3f3127d8c5ead074aede1a2e
maho0628 <149309001+maho0628@users.noreply.github.com>
2026-01-30 03:05:04 +0900

    エラー修正

--- Assets/yu-ki-rohi/Code/Scripts/InGame/Systems/GameTimeManager.cs
+++ Assets/yu-ki-rohi/Code/Scripts/InGame/Systems/GameTimeManager.cs
@@ -43,7 +43,7 @@ public class GameTimeManager : MonoBehaviour
     private void CallOfAproachingFinish()
     {
         // TODO: タイムアップ30秒前通知音
-        AudioManager.Instance.PlayBGMIfNotPlaying(SEName.TimeUpWarning);
+        AudioManager.Instance.PlayBGMIfNotPlaying(BGMName.TimeUpWarning);
     }
 
     private void TimeOver()
