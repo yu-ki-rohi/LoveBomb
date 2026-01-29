1d9d24d9f795ea48ca4c37b1a58bd4dae5059904
maho0628 <149309001+maho0628@users.noreply.github.com>
2026-01-29 19:52:41 +0900

    最小のコードでボタンにつけるだけでマウスオーバーとクリック時のSEが鳴るスクリプト実装

---
+++ Assets/Yamashina/Code/SE/UIButtonSE.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+using UnityEngine.EventSystems;
+
+public class UIButtonSE : MonoBehaviour,
+    IPointerClickHandler,
+    IPointerEnterHandler
+{
+
+    public void OnPointerClick(PointerEventData e)
+    {
+        AudioManager.Instance.PlaySEById(SEName.Click); ;
+    }
+
+    public void OnPointerEnter(PointerEventData e)
+    {
+        AudioManager.Instance.PlaySEById(SEName.MouseOver);
+    }
+}
+
