84386fd099a5b02731fd44e21820919c71e07445
maho0628 <149309001+maho0628@users.noreply.github.com>
2026-01-29 23:45:56 +0900

    タイムアップ時のSE

--- Assets/yu-ki-rohi/Code/Scripts/InGame/Systems/GameTimeManager.cs
+++ Assets/yu-ki-rohi/Code/Scripts/InGame/Systems/GameTimeManager.cs
@@ -37,6 +37,7 @@ public class GameTimeManager : MonoBehaviour
     {
         StopAllCoroutines();
         DebugMessenger.Log("Time Up!!");
+        AudioManager.Instance.PlaySEById(SEName.TimeUp);
         OnTimeUp?.Invoke();
     }
 
